c601ac443575ab84a52d449ab4f1f32156a45f65
Code Gnomic <codegnomic@proton.me>
2022-12-21 11:47:43 -0500

    added theme wrapper to add darkmode feature

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
+    import Icon from "@iconify/svelte"
+    import sunDuotone from "@iconify/icons-ph/sun-duotone"
+    import moonStarsDuotone from "@iconify/icons-ph/moon-stars-duotone"
     import NavItem from "./NavItem.svelte"
+    import { darkmode } from "src/stores"
+
+    const handleDarkmodeToggle = () => {
+        darkmode.set(!$darkmode)
+    }
 </script>
 
-<nav class="w-full flex bg-slate-300 dark:bg-slate-900 p-1">
+<nav class="w-full flex bg-slate-300 dark:bg-slate-900 dark:text-slate-100 p-1">
     <ul class="flex-grow flex justify-center">
         <NavItem slug="/" title="Home" />
         <NavItem slug="/blog/1" title="Blog" />
         <NavItem slug="/projects" title="Projects" />
         <NavItem slug="/contact" title="Contact" />
     </ul>
+    <button on:click={handleDarkmodeToggle}>
+        {#if $darkmode}
+            <Icon icon={sunDuotone} />
+        {:else}
+            <Icon icon={moonStarsDuotone} />
+        {/if}
+    </button>
 </nav>
---
+++ src/components/ThemeWrapper.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { darkmode } from "src/stores"
+</script>
+
+<div class={`${$darkmode ? "dark" : ""} w-screen h-screen flex flex-col`}>
+    <slot />
+</div>
